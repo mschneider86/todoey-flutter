179a51cb1edcb7a2104eb204602c247766d26dc5
Matheus Schneider <matheus.ssm@gmail.com>
2020-11-10 19:13:26 -0300

    added: callback function to update checkbox state

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -8,6 +8,12 @@ class TaskTile extends StatefulWidget {
 class _TaskTileState extends State<TaskTile> {
   bool isChecked = false;
 
+  void checkboxCallback(bool checkboxState) {
+    setState(() {
+      isChecked = checkboxState;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
@@ -16,26 +22,24 @@ class _TaskTileState extends State<TaskTile> {
         style: TextStyle(
             decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
-      trailing: TaskCheckbox(isChecked),
+      trailing: TaskCheckbox(
+          checkboxState: isChecked, toggleCheckboxState: checkboxCallback),
     );
   }
 }
 
 class TaskCheckbox extends StatelessWidget {
   final bool checkboxState;
+  final Function toggleCheckboxState;
 
-  TaskCheckbox(this.checkboxState);
+  TaskCheckbox({this.checkboxState, this.toggleCheckboxState});
 
   @override
   Widget build(BuildContext context) {
     return Checkbox(
       activeColor: Colors.lightBlueAccent,
       value: checkboxState,
-      onChanged: (newValue) {
-        // setState(() {
-        //   isChecked = newValue;
-        // });
-      },
+      onChanged: toggleCheckboxState,
     );
   }
 }
