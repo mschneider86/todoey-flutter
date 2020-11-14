b7aa08c5c914e6b35460f5f6d117f39c38005686
Matheus Schneider <matheus.ssm@gmail.com>
2020-11-14 12:45:06 -0300

    updated: refactored to add tasks using provider

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todoey_flutter/models/task_data.dart';
 
 class AddTaskScreen extends StatelessWidget {
-
-  final Function addTaskCallback;
-
-  AddTaskScreen(this.addTaskCallback);
 
   @override
   Widget build(BuildContext context) {
@@ -49,7 +47,8 @@ class AddTaskScreen extends StatelessWidget {
               ),
               color: Colors.lightBlueAccent,
               onPressed: () {
-addTaskCallback(newTaskTitle);
+                Provider.of<TaskData>(context).addTask(newTaskTitle);
+                Navigator.pop(context);
               },
             ),
           ],
